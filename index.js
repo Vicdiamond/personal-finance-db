c43ccd9f71e84fcd665f567dbf8118b8a97f14cc
Dev <97352849+Vicdiamond@users.noreply.github.com>
2025-01-06 01:16:09 +0100

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
-const router = jsonServer.router('data.json') // Update this path if needed
+const router = jsonServer.router('db.json') // Update this path if needed
 const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 5000
 
